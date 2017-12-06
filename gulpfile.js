b439dca88efb990ab7cc4cb7aa559ddc41f21cc4
Katherine Johnson <johnsk72@ferris.edu>
2017-12-05 19:45:18 -0500

    More partials, css organization, and gulp downloads

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,20 @@
+var minifycss = require('gulp-minify-css');
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var notifier = require('node-notifier');
+var notify = require('gulp-notify');
 
 gulp.task('sass', function() {
     return gulp.src('src/scss/style.scss')
-        .pipe(sass())
+        .pipe(plumber())
+        .pipe(sass()
+            .on("error", notify.onError({
+                title: "Sass Error",
+                message: "Error: <%= error.message %>"
+            }))
+        )
+        .pipe(minifycss())
         .pipe(gulp.dest('assets/css'));
 });
 
